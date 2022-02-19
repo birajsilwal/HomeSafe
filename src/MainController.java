457c02059593c2e601193407128803dc248eb379
NGOC TA <ngocta0905@gmail.com>
2022-02-18 17:08:50 -0700

    create Ruby's branch

--- src/MainController.java
+++ src/MainController.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 public class MainController extends Application {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("ruby is here");
         launch(args);
     }
 
