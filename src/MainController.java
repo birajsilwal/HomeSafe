0c6d84b4bd13424ad3b3f18fae475d1a4e9ca7fc
Nehemiah Cionelo <ncionelo@unm.edu>
2022-02-20 16:09:23 -0700

    start

--- src/MainController.java
+++ src/MainController.java
@@ -25,6 +25,7 @@ public class MainController extends Application {
         System.out.println("hello");
         System.out.println("Hey can you accept my change?");
         System.out.println("ruby is here");
+        System.out.println("nemo was here");
         launch(args);
     }
 
