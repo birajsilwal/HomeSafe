077a22acfeddf0df3df25d20b85721069778b827
Nehemiah Cionelo <nemocionelo@gmail.com>
2022-02-20 19:49:26 -0700

    Changed VerifyFingerprint.java and VerifyPassword.java to FingerprintKey and PasswordKey. AuthenticationManager written around abstract "key" that these two methods use. Added possible Fingerprint class for "fingerprint"? Reset PINs are generated exclusively in PasswordKey.

---
+++ src/FingerprintKey.java
@@ -0,0 +1,9 @@
+/**
+ * FingerprintKey class extends AuthenticationManager and manages a
+ * key of a fingerprint for accessing the safe
+ * @author Nehemiah Cionelo
+ */
+
+public class FingerprintKey extends AuthenticationManager {
+    public FingerprintKey () {}
+}
