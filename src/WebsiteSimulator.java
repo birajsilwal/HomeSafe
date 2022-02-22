b095d271c889ec5097479ebc384426833706970d
Laxman Adhikari <ladhikari023@unm.edu>
2022-02-22 15:36:14 -0700

    Website Simulator Implemented

---
+++ src/WebsiteSimulator.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class WebsiteSimulator {
+    private HashMap<String,String> serialToPhoneNumber = new HashMap<>();
+    private HashMap<String, String> serialToSetUp = new HashMap<>();
+    private HashMap<String, Stack<String>> serialToResetPins = new HashMap<>();
+
+    public WebsiteSimulator() {}
+
+    public void registerPhoneNumber(String phone, String serial){
+        serialToPhoneNumber.put(phone,serial);
+    }
+    public void setSerialToResetPins(String serial, Stack<String> reset_pins){
+        serialToResetPins.put(serial,reset_pins);
+    }
+    public void setSerialToSetUp(String serial, String setup){
+        serialToSetUp.put(serial,setup);
+    }
+    public String getSetUpPin(String phone){
+        String serial = serialToPhoneNumber.get(phone);
+        return serialToSetUp.get(serial);
+    }
+    public String getResetPin(String phone){
+        String serial = serialToPhoneNumber.get(phone);
+        return serialToResetPins.get(serial).pop();
+    }
+
+}
