bfef2dc91a7464fcd1ea6312a663cb0d1bba82d7
Laxman Adhikari <ladhikari023@unm.edu>
2022-02-22 14:16:50 -0700

    Laxman checked integration

--- src/AuthenticationManager.java
+++ src/AuthenticationManager.java
@@ -1,3 +1,17 @@
-abstract class AuthenticationManager {
+ class AuthenticationManager {
+    private static String savedPassword = "123456";
+    private String fingerPrint = "";
 
+    public void setSavedPassword(String s){
+        this.savedPassword = s;
+    }
+    public void setFingerPrint(String s){
+        this.fingerPrint = s;
+    };
+
+    public  boolean verifyPassword(String entered_password){
+        System.out.println(savedPassword);
+        return entered_password.equals(savedPassword);
+    };
+    //public  boolean verifyFingerPrint(String fingerPrint);
 }
--- src/InputController.java
+++ src/InputController.java
@@ -1,3 +1,20 @@
-public class InputController {
+import java.util.Scanner;
 
+public class InputController extends AuthenticationManager{
+    String entered_password = "";
+    String finger_print = "";
+
+    public void readKey(String s){
+        this.entered_password += s;
+    }
+    public void finger_print(String s){
+        this.finger_print = s;
+    }
+
+    public boolean checkPassword() {
+        return verifyPassword(entered_password);
+    }
+    public boolean checkResetPin(){
+        return false;
+    }
 }
