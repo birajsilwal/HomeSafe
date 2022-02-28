42d32326b89f2aedf5ae9cffdee348edf2f1525a
Biraj Silwal <birajsilwal.ai@gmail.com>
2022-02-27 17:30:41 -0700

    get and set methods to change the state... of sensors and lock system

--- src/SecurityManager.java
+++ src/SecurityManager.java
@@ -1,3 +1,41 @@
+/**
+ * Security Manager class handles status of lock state and
+ * sensor state.
+ * When the lock system is locked, the sensor is presses
+ * but when the lock system is unlocked, the sensor is unpressed state.
+ * @author Biraj Silwal
+ */
+
 public class SecurityManager {
 
+    private Boolean lockedState = true;
+    private Boolean sensorIsPressed = true;
+
+    /**
+     * set the state of the lock system to locked state.
+     */
+    public void setLockedState(Boolean lockedState) {
+        this.lockedState = lockedState;
+    }
+
+    /**
+     * @return lockedState - lock state of the lock system
+     */
+    public Boolean getLockedState() {
+        return lockedState;
+    }
+
+    /**
+     * if the lock system is locked, the sensor is pressed
+     */
+    public void setSensorPressedState(Boolean lockedState) {
+        sensorIsPressed = lockedState;
+    }
+
+    /**
+     * @return sensorIsPressed, if the sensor is pressed, return true else false
+     */
+    public Boolean getSensorPressedState() {
+        return sensorIsPressed;
+    }
 }
